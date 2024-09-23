e2d127d0e7b36a2fba5d0e3aea6a052e314d263c
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-09-23 08:18:18 -0600

    clase persona

---
+++ Corte1/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Persona
+{
+    public string Nombres { get; set; }
+    public string Apellidos { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string Ciudad { get; set; }
+
+    public Persona(string nombres, string apellidos, DateTime fechaNacimiento, string ciudad)
+    {
+        Nombres = nombres;
+        Apellidos = apellidos;
+        FechaNacimiento = fechaNacimiento;
+        Ciudad = ciudad;
+    }
+}
