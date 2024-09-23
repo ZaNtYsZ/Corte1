218facf8f5b50f24f00298c4f6ddb241e32bfd03
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-09-23 08:29:24 -0600

    clase operacion

---
+++ Corte1/Operacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Operacion
+{
+    public int CalcularEdad(Persona persona)
+    {
+        DateTime hoy = DateTime.Now;
+        int edad = hoy.Year - persona.FechaNacimiento.Year;
+
+        if (persona.FechaNacimiento.Date > hoy.AddYears(-edad)) edad--;
+
+        return edad;
+    }
+
+    public bool EsMayorDeEdad(Persona persona)
+    {
+        return CalcularEdad(persona) >= 18;
+    }
+}
+
